dda1ee8ce781f3a3496e6f10b25e083671c51bdb
werolksss <darialaf@icloud.com>
2025-10-23 15:40:38 +0900

    31.10

--- dz.cpp
+++ dz.cpp
@@ -1,105 +1,163 @@
-﻿/*
-1. C) 22
-2. A) char
-3. A) const, D) constexpr
-4. A) x++;
-5. C) if(x==10){...}
-6. B) Альтернативный способ записи условного выражения вида "если ..., иначе..."
-7. A) Вход данных из потока ввода
-8. C) do-while
-9. C) Целочисленные значения и символы
-10. B) Для обработки двумерных структур данных (например, матриц)
-*/
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+﻿#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "ru");
-
-    //1
-    cout << "задание 1:\n";
-    int a, b, c;
-    cout << "Введите три числа: ";
-    cin >> a >> b >> c;
-
-    int max = a;
-
-    if (b > max) {
-        max = b;
-    }
-    if (c > max) {
-        max = c;
-    }
-
-    cout << "Наибольшее число: " << max << "\n\n";
-
-    //2
-    cout << "задание 2:\n";
-    int n;
-    cout << "Введите размер таблицы: ";
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cout << i * j << " ";
+    // 1
+    cout << "Задание 1\n";
+    int size = 8;
+    double arr1[8] = { -5.7, 6.0, 2, 0, -4.7, 6, 8.1, -4 };
+    double pos = 0; 
+    double neg = 0; 
+    for (int a = size - 1; a >= 0; a--)
+    {
+        if (arr1[a] > 0)
+        {
+            pos = arr1[a];
+            break;
         }
-        cout << "\n";
+    }
+    for (int b = 0; b < size; b++)
+    {
+        if (arr1[b] < 0)
+        {
+            neg = arr1[b];
+            break;
+        }
+    }
+    cout << "Последнее положительное: " << pos << "\n";
+    cout << "Первое отрицательное: " << neg << "\n\n";
+
+    // 2
+    cout << "Задание 2\n";
+    int arr2[10];
+    cout << "Введите количество жильцов в 10 квартирах:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr2[i];
+    }
+    // а)
+    int q;
+    cout << "Введите номер квартиры (1-10): ";
+    cin >> q;
+    q--;
+    cout << "Жильцов: " << arr2[q] << "\n";
+    if (q % 2 == 0) cout << "Сосед: квартира " << q + 2 << " жильцов: " << arr2[q + 1] << "\n";
+    else cout << "Сосед: квартира " << q << " жильцов: " << arr2[q - 1] << "\n";
+    // б)
+    cout << "\nСумма жильцов по подъездам:\n";
+    int pod = 0;
+    int k = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        pod += arr2[i];
+        k++;
+        if (k == 2)
+        {
+            cout << pod << "\n";
+            pod = 0;
+            k = 0;
+        }
+    }
+
+    // в)
+    cout << "\nМногодетные семьи:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr2[i] > 5)
+        {
+            cout << "Квартира: " << i + 1 << "\n";
+        }
     }
     cout << "\n";
 
     //3
-    cout << "задание 3:\n";
-    int x;
-    cout << "Введите число: ";
-    cin >> x;
-
-    if (x % 3 == 0 && x % 5 == 0) {
-        cout << "FizzBuzz\n\n";
-    }
-    else if (x % 3 == 0) {
-        cout << "Fizz\n\n";
-    }
-    else if (x % 5 == 0) {
-        cout << "Buzz\n\n";
-    }
-    else {
-        cout << x << "\n\n";
-    }
-
+    cout << "Задание 3\n";
+    int arr3[10];
+    cout << "Введите температуру за 10 дней января:\n";
+    for (int i = 0; i < 10; i++)
+        cin >> arr3[i];
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+        sum += arr3[i];
+    cout << "Средняя температура: " << sum / 10.0 << "\n";
+    int t, cnt = 0;
+    cout << "Введите метку температуры: ";
+    cin >> t;
+    for (int i = 0; i < 10; i++)
+        if (arr3[i] < t) cnt++;
+    cout << "Температура опускалась ниже " << t << ": " << cnt << " раз\n\n";
+   
     //4
-    cout << "задание 4:\n";
-    int num;
-    cout << "Введите число: ";
-    cin >> num;
+    cout << "Задание 4\n";
+    int arr4[10];
+    cout << "Введите стоимость 10 авто:\n";
+    for (int i = 0; i < 10; i++)
+        cin >> arr4[i];
 
-    long factorial = 1;
+    int max = arr4[0];
+    for (int i = 1; i < 10; i++)
+        if (arr4[i] > max) max = arr4[i];
 
-    for (int i = 1; i <= num; i++) {
-        factorial = factorial * i;
-    }
-
-    cout << "Факториал: " << factorial << "\n\n";
-
-    //6
-    cout << "задание 6:\n";
-    int N;
-    cout << "Введите число: ";
-    cin >> N;
-
-    for (int i = 1; i <= N; i++) {
-        if (i % 15 == 0) {
-            cout << "ThreeFive ";
+    cout << "Максимальная стоимость: " << max << "\n";
+    // а) первый
+    for (int i = 0; i < 10; i++)
+        if (arr4[i] == max)
+        {
+            cout << "Первый номер: " << i + 1 << "\n";
+            break;
         }
-        else if (i % 3 == 0) {
-            cout << "Three ";
+    // б) последний
+    for (int i = 9; i >= 0; i--)
+        if (arr4[i] == max)
+        {
+            cout << "Последний номер: " << i + 1 << "\n";
+            break;
         }
-        else {
-            cout << i << " ";
+    cout << "\n";
+
+    //5
+    cout << "Задание 5\n";
+    int arr5[10];
+    cout << "Введите 10 чисел:\n";
+    for (int i = 0; i < 10; i++)
+        cin >> arr5[i];
+    cout << "Повторяющиеся элементы:\n";
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = i + 1; j < 10; j++)
+        {
+            if (arr5[i] == arr5[j])
+            {
+                cout << arr5[i] << "\n";
+                break;
+            }
         }
     }
     cout << "\n";
-
+    //6
+    cout << "Задание 6\n";
+    int A[10], B[10], X[20];
+    cout << "Введите массив A:\n";
+    for (int i = 0; i < 10; i++) cin >> A[i];
+    cout << "Введите массив B:\n";
+    for (int i = 0; i < 10; i++) cin >> B[i];
+    // а) чередование
+    cout << "Массив X (чередование):\n";
+    int z = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        X[z++] = A[i];
+        X[z++] = B[i];
+    }
+    for (int i = 0; i < 20; i++) cout << X[i] << " ";
+    cout << "\n";
+    // б) следование
+    cout << "Массив X (следование):\n";
+    for (int i = 0; i < 10; i++) X[i] = A[i];
+    for (int i = 0; i < 10; i++) X[i + 10] = B[i];
+    for (int i = 0; i < 20; i++) cout << X[i] << " ";
+    cout << "\n";
     return 0;
 }
