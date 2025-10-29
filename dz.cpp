4bfdce464b531b611832633bb3d7352f7732d809
werolksss <darialaf@icloud.com>
2025-10-29 22:30:01 +0900

    Контрольная работа по оп на C++

--- dz.cpp
+++ dz.cpp
@@ -1,149 +1,71 @@
 ﻿#include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    //1 задание
-    cout << "задание 1\n";
-    int a;
-    cout << "введите число: ";
+    // 2
+    cout << "умножение\n";
+    int a, b;
+    cout << "введите первое число: ";
     cin >> a;
+    cout << "введите второе число: ";
+    cin >> b;
+    int c = a * b;
+    cout << "произведение: " << c << "\n\n";
 
-    int massiv1[3][3];
-    int b = a;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            massiv1[i][j] = b;
-            b = b * 2; 
-        }
-    }
-
-    cout << "полученный массив:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << massiv1[i][j] << " ";
-        }
-        cout << "\n";
-    }
-    cout << "\n";
-
-    //2 задание
-    cout << "задание 2\n";
-    int c;
+    // 4
+    cout << "четность\n";
+    int d;
     cout << "введите число: ";
-    cin >> c;
+    cin >> d;
+    if (d % 2 == 0) {
+        cout << "четное\n\n";
+    }
+    else {
+        cout << "нечетное\n\n";
+    }
 
-    int massiv2[3][3]; 
-    int d = c;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            massiv2[i][j] = d;
-            d = d + 1;
+    // 7
+    cout << "кубы чисел\n";
+    for (int e = 1; e <= 10; e++) {
+        int f = e * e * e;
+        cout << "куб числа " << e << " = " << f << "\n";
+    }
+    cout << "\n";
+
+    // 11
+	const int size = 8;
+    int g[size] = { 45, 12, 78, 23, 56, 89, 34, 67 };
+    int h = g[0];
+
+    for (int i = 1; i < size; i++) {
+        if (g[i] > h) {
+            h = g[i];
+        }
+    }
+    cout << "максимальный элемент: " << h << "\n\n";
+
+    // 13
+    cout << "двумерный массив 3х3\n";
+    const int j = 3;
+    int jkk[j][j]; // Объявление массива
+    int k = 1;
+
+    for (int l = 0; l < j; l++) {
+        for (int m = 0; m < j; m++) {
+            jkk[l][m] = k;
+            k++;
         }
     }
 
-  
-    cout << "полученный массив:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << massiv2[i][j] << " ";
+    for (int n = 0; n < j; n++) {
+        for (int o = 0; o < j; o++) {
+            cout << jkk[n][o] << " ";
         }
         cout << "\n";
     }
     cout << "\n";
-
-    //3
-    cout << "задание 3\n";
-    srand(time(0)); 
-
-    int massiv3[2][6]; 
-    cout << "исходный массив:\n";
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 6; j++) {
-            massiv3[i][j] = rand() % 10; 
-            cout << massiv3[i][j] << " ";
-        }
-        cout << "\n";
-    }
-
-    int sdvig;
-    cout << "введите количество сдвигов: ";
-    cin >> sdvig;
-
-    int napravlenie;
-    cout << "выберите направление (1-влево, 2-вправо, 3-вверх, 4-вниз): ";
-    cin >> napravlenie;
-    int temp[2][6];
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 6; j++) {
-            temp[i][j] = massiv3[i][j];
-        }
-    }
-    if (napravlenie == 1) {
-        for (int s = 0; s < sdvig; s++) {
-            for (int i = 0; i < 2; i++) {
-                int first = temp[i][0];
-                for (int j = 0; j < 5; j++) {
-                    temp[i][j] = temp[i][j + 1];
-                }
-                temp[i][5] = first;
-            }
-        }
-    }
-    else if (napravlenie == 2) {
-        for (int s = 0; s < sdvig; s++) {
-            for (int i = 0; i < 2; i++) {
-                int last = temp[i][5];
-                for (int j = 5; j > 0; j--) {
-                    temp[i][j] = temp[i][j - 1];
-                }
-                temp[i][0] = last;
-            }
-        }
-    }
-    else if (napravlenie == 3) {
-        for (int s = 0; s < sdvig; s++) {
-            int first_row[6];
-            for (int j = 0; j < 6; j++) {
-                first_row[j] = temp[0][j];
-            }
-            for (int i = 0; i < 1; i++) {
-                for (int j = 0; j < 6; j++) {
-                    temp[i][j] = temp[i + 1][j];
-                }
-            }
-            for (int j = 0; j < 6; j++) {
-                temp[1][j] = first_row[j];
-            }
-        }
-    }
-    else if (napravlenie == 4) { 
-        for (int s = 0; s < sdvig; s++) {
-            int last_row[6];
-            for (int j = 0; j < 6; j++) {
-                last_row[j] = temp[1][j];
-            }
-            for (int i = 1; i > 0; i--) {
-                for (int j = 0; j < 6; j++) {
-                    temp[i][j] = temp[i - 1][j];
-                }
-            }
-            for (int j = 0; j < 6; j++) {
-                temp[0][j] = last_row[j];
-            }
-        }
-    }
-    cout << "Массив после сдвига:\n";
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 6; j++) {
-            cout << temp[i][j] << " ";
-        }
-        cout << "\n";
-    }
 
     return 0;
 }
