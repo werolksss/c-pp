fadcd52aac91969f0907db0aab0541d35dd44d79
werolksss <darialaf@icloud.com>
2025-11-19 16:15:11 +0900

    шаблоны функции

--- dz.cpp
+++ dz.cpp
@@ -1,169 +1,130 @@
 ﻿#include <iostream>
 using namespace std;
 
-int stepen(int a, int b)
-{
-    if (b == 0)
-        return 1;
-    else
-        return a * stepen(a, b - 1);
-}
-
-void zvezdi(int n)
-{
-    if (n > 0)
-    {
-        cout << "*";
-        zvezdi(n - 1);
-    }
-}
-
-int summa(int a, int b)
-{
-    if (a > b)
-        return 0;
-    if (a == b)
-        return a;
-    else
-        return a + summa(a + 1, b);
-}
-
-int findMinSum(int arr[], int size, int k, int& minSum, int& startIndex, int currentIndex)
-{
-    if (currentIndex > size - k)
-        return startIndex;
-
-    int currentSum = 0;
-    for (int i = 0; i < k; i++)
-    {
-        currentSum += arr[currentIndex + i];
-    }
-
-    if (currentSum < minSum)
-    {
-        minSum = currentSum;
-        startIndex = currentIndex;
-    }
-
-    return findMinSum(arr, size, k, minSum, startIndex, currentIndex + 1);
-}
-
-int findMax(int arr[], int size)
-{
-    int max = arr[0];
-    for (int i = 1; i < size; i++)
-    {
-        if (arr[i] > max)
-            max = arr[i];
-    }
-    return max;
-}
-
-int findMax(int arr[][3], int rows, int cols)
-{
-    int max = arr[0][0];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if (arr[i][j] > max)
-                max = arr[i][j];
+void initMatrix(int m[][10], int s) {
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            m[i][j] = rand() % 100;
         }
     }
-    return max;
 }
 
-int findMax(int arr[][2][2], int x, int y, int z)
-{
-    int max = arr[0][0][0];
-    for (int i = 0; i < x; i++)
-    {
-        for (int j = 0; j < y; j++)
-        {
-            for (int k = 0; k < z; k++)
-            {
-                if (arr[i][j][k] > max)
-                    max = arr[i][j][k];
+void printMatrix(int m[][10], int s) {
+    for (int i = 0; i < s; i++) {
+        for (int j = 0; j < s; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void findDiagonal(int m[][10], int s, int& max, int& min) {
+    max = m[0][0];
+    min = m[0][0];
+    for (int i = 0; i < s; i++) {
+        if (m[i][i] > max) max = m[i][i];
+        if (m[i][i] < min) min = m[i][i];
+    }
+}
+
+void sortRow(int r[], int s) {
+    for (int i = 0; i < s - 1; i++) {
+        for (int j = 0; j < s - i - 1; j++) {
+            if (r[j] > r[j + 1]) {
+                int t = r[j];
+                r[j] = r[j + 1];
+                r[j + 1] = t;
             }
         }
     }
-    return max;
 }
 
-int findMax(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
+void sortMatrix(int m[][10], int s) {
+    for (int i = 0; i < s; i++) {
+        sortRow(m[i], s);
+    }
 }
 
-int findMax(int a, int b, int c)
-{
-    int max = a;
-    if (b > max)
-        max = b;
-    if (c > max)
-        max = c;
-    return max;
+int nod(int a, int b) {
+    if (b == 0) return a;
+    return nod(b, a % b);
 }
 
-int main()
-{
-    setlocale(LC_ALL, "ru");
+void checkNumber(int s[], int a[], int p, int& b, int& c) {
+    if (p >= 4) return;
 
-    cout << "1. степень числа\n";
-    int num, pow;
-    cout << "введите число: ";
-    cin >> num;
-    cout << "введите степень: ";
-    cin >> pow;
-    cout << num << " в степени " << pow << " = " << stepen(num, pow) << "\n\n";
-
-    cout << "2. звезды в ряд\n";
-    int n;
-    cout << "введите количество звезд: ";
-    cin >> n;
-    cout << "результат: ";
-    zvezdi(n);
-    cout << "\n\n";
-
-    cout << "3. сумма чисел в диапазоне\n";
-    int a, b;
-    cout << "введите начало диапазона (a): ";
-    cin >> a;
-    cout << "введите конец диапазона (b): ";
-    cin >> b;
-    cout << "сумма чисел от " << a << " до " << b << " = " << summa(a, b) << "\n\n";
-
-    cout << "4. поиск минимальной суммы\n";
-    int arr[100];
-    for (int i = 0; i < 100; i++)
-    {
-        arr[i] = rand() % 100;
+    if (a[p] == s[p]) {
+        b++;
+    }
+    else {
+        for (int i = 0; i < 4; i++) {
+            if (a[p] == s[i]) {
+                c++;
+                break;
+            }
+        }
     }
 
-    int minSum = 1000;
-    int startIndex = 0;
-    int k = 10;
+    checkNumber(s, a, p + 1, b, c);
+}
 
-    int result = findMinSum(arr, 100, k, minSum, startIndex, 0);
-    cout << "начало последовательности с минимальной суммой: " << result << "\n";
-    cout << "минимальная сумма: " << minSum << "\n\n";
+void game(int s[], int t) {
+    int n;
+    cout << "попытка " << t << ": ";
+    cin >> n;
 
-    cout << "5. перегруженные функции\n";
+    int a[4];
+    a[0] = n / 1000;
+    a[1] = (n / 100) % 10;
+    a[2] = (n / 10) % 10;
+    a[3] = n % 10;
 
-    int arr1D[5] = { 3, 7, 2, 9, 1 };
-    cout << "максимум в одномерном массиве: " << findMax(arr1D, 5) << "\n";
+    int b = 0, c = 0;
+    checkNumber(s, a, 0, b, c);
 
-    int arr2D[2][3] = { {3, 7, 2}, {9, 1, 5} };
-    cout << "максимум в двумерном массиве: " << findMax(arr2D, 2, 3) << "\n";
+    cout << "быки: " << b << ", коровы: " << c << endl;
 
-    int arr3D[2][2][2] = { {{3, 7}, {2, 9}}, {{1, 5}, {8, 4}} };
-    cout << "максимум в трехмерном массиве: " << findMax(arr3D, 2, 2, 2) << "\n";
+    if (b == 4) {
+        cout << "угадал за " << t << " попыток!" << endl;
+        return;
+    }
 
-    cout << "максимум из 5 и 3: " << findMax(5, 3) << "\n";
-    cout << "максимум из 5, 3 и 8: " << findMax(5, 3, 8) << "\n";
+    game(s, t + 1);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    srand(time(0));
+
+    cout << "1. матрицы\n";
+    int m[10][10];
+    int s = 4;
+
+    initMatrix(m, s);
+    printMatrix(m, s);
+
+    int max, min;
+    findDiagonal(m, s, max, min);
+    cout << "макс: " << max << ", мин: " << min << endl;
+
+    sortMatrix(m, s);
+    cout << "после сортировки:\n";
+    printMatrix(m, s);
+    cout << endl;
+
+    cout << "2. нод\n";
+    int a, b;
+    cin >> a >> b;
+    cout << "НОД: " << nod(a, b) << endl << endl;
+
+    cout << "3. быки и коровы\n";
+    int secret[4];
+    secret[0] = rand() % 9 + 1;
+    for (int i = 1; i < 4; i++) {
+        secret[i] = rand() % 10;
+    }
+
+    game(secret, 1);
 
     return 0;
 }
