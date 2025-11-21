8397f4745dd0a95214409ecf79fe37a06df5939d
werolksss <darialaf@icloud.com>
2025-11-21 14:07:37 +0900

    Сортировка

--- dz.cpp
+++ dz.cpp
@@ -1,90 +1,175 @@
 ﻿#include <iostream>
-#include <cmath>
 using namespace std;
-
-template <typename T>
-T srednee(T arr[], int size) {
-    T sum = 0;
-    for (int i = 0; i < size; i++) sum += arr[i];
-    return sum / size;
-}
-
-template <typename T>
-void uravnenie(T a, T b) {
-    if (a == 0) cout << (b == 0 ? "бесконечно много решений\n" : "нет решений\n");
-    else cout << "корень: x = " << -b / a << "\n";
-}
-
-template <typename T>
-void uravnenie(T a, T b, T c) {
-    if (a == 0) { uravnenie(b, c); return; }
-
-    T d = b * b - 4 * a * c;
-    if (d > 0) cout << "два корня: x1 = " << (-b + sqrt(d)) / (2 * a) << ", x2 = " << (-b - sqrt(d)) / (2 * a) << "\n";
-    else if (d == 0) cout << "один корень: x = " << -b / (2 * a) << "\n";
-    else cout << "действительных корней нет\n";
-}
-
-double okruglenie(double chislo, int znakov) {
-    return round(chislo * pow(10, znakov)) / pow(10, znakov);
-}
-
-template <typename T>
-T maximum(T arr[], int size) {
-    T max = arr[0];
-    for (int i = 1; i < size; i++) if (arr[i] > max) max = arr[i];
-    return max;
-}
-
-template <typename T>
-T maximum(T arr[][10], int rows, int cols) {
-    T max = arr[0][0];
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            if (arr[i][j] > max) max = arr[i][j];
-    return max;
-}
-
-template <typename T>
-T maximum(T arr[][10][10], int x, int y, int z) {
-    T max = arr[0][0][0];
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
-            for (int k = 0; k < z; k++)
-                if (arr[i][j][k] > max) max = arr[i][j][k];
-    return max;
-}
-
-int maximum(int a, int b) { return a > b ? a : b; }
-int maximum(int a, int b, int c) { return maximum(maximum(a, b), c); }
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    cout << "1 задание: среднее арифметическое массива\n";
-    int arr1[] = { 1, 2, 3, 4, 5 };
-    cout << "среднее: " << srednee(arr1, 5) << "\n\n";
+    //пузырьковая сортировка
+    cout << "задание 1: пузырьковая сортировка\n";
+    int a[5] = { 5, 2, 8, 1, 9 };
+    int n = 5;
+    cout << "исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    cout << "отсортированный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "\n\n";
+    // сортировка вставками
+    cout << "задание 2: сортировка вставками\n";
+    int b[5] = { 5, 2, 8, 1, 9 };
 
-    cout << "2 задание: решение уравнений\n";
-    cout << "2x + 4 = 0: "; uravnenie(2.0, 4.0);
-    cout << "x^2 - 5x + 6 = 0: "; uravnenie(1.0, -5.0, 6.0);
+    cout << "исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << b[i] << " ";
+    }
     cout << "\n";
 
-    cout << "3 задание: округление числа\n";
-    cout << "3.14159 до 2 знаков: " << okruglenie(3.14159, 2) << "\n\n";
+    for (int i = 1; i < n; i++) {
+        int key = b[i];
+        int j = i - 1;
+        while (j >= 0 && b[j] > key) {
+            b[j + 1] = b[j];
+            j--;
+        }
+        b[j + 1] = key;
+    }
 
-    cout << "4 задание: максимальное значение в массивах\n";
-    int odnomerniy[] = { 1, 8, 3, 6, 2 };
-    int dvumerniy[3][10] = { {1, 2, 3}, {9, 5, 6}, {7, 8, 4} };
-    int trehmerniy[2][10][10] = { {{1, 2}, {3, 4}}, {{15, 6}, {7, 8}} };
+    cout << "отсортированный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << b[i] << " ";
+    }
+    cout << "\n\n";
 
-    cout << "в одномерном: " << maximum(odnomerniy, 5) << "\n";
-    cout << "в двумерном: " << maximum(dvumerniy, 3, 3) << "\n";
-    cout << "в трехмерном: " << maximum(trehmerniy, 2, 2, 2) << "\n\n";
+    //успеваемость
+    cout << "задание 3: успеваемость\n";
+    int m[10];
 
-    cout << "5 задание: максимальное значение целых чисел\n";
-    cout << "из 5 и 10: " << maximum(5, 10) << "\n";
-    cout << "из 3, 7 и 2: " << maximum(3, 7, 2) << "\n";
+    cout << "введите 10 оценок:\n";
+    for (int i = 0; i < 10; i++) {
+        cin >> m[i];
+    }
+
+    int ch;
+    do {
+        cout << "\n1 - оценки\n2 - пересдача\n3 - стипендия\n0 - выход\n";
+        cin >> ch;
+
+        if (ch == 1) {
+            for (int i = 0; i < 10; i++) {
+                cout << m[i] << " ";
+            }
+            cout << "\n";
+        }
+        else if (ch == 2) {
+            int num, nm;
+            cout << "номер и новая оценка: ";
+            cin >> num >> nm;
+            if (num >= 1 && num <= 10) {
+                m[num - 1] = nm;
+            }
+        }
+        else if (ch == 3) {
+            float s = 0;
+            for (int i = 0; i < 10; i++) {
+                s += m[i];
+            }
+            float avg = s / 10;
+            cout << "средний бал: " << avg << "\n";
+            if (avg >= 10.7) {
+                cout << "стипендия есть\n";
+            }
+            else {
+                cout << "стипендии нет\n";
+            }
+        }
+    } while (ch != 0);
+    //быстрая сортировка
+    cout << "\nзадание 4: быстрая сортировка\n";
+    int c[5] = { 5, 2, 8, 1, 9 };
+    cout << "исходный массив: ";
+    for (int i = 0; i < 5; i++) {
+        cout << c[i] << " ";
+    }
+    cout << "\n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (c[i] > c[j]) {
+                int t = c[i];
+                c[i] = c[j];
+                c[j] = t;
+            }
+        }
+    }
+    cout << "отсортированный массив: ";
+    for (int i = 0; i < 5; i++) {
+        cout << c[i] << " ";
+    }
+    cout << "\n\n";
+
+    //сложная сортировка
+    cout << "задание 5: сложная сортировка\n";
+    int d[6] = { 3, -1, 7, 2, 8, 4 };
+
+    cout << "исходный массив: ";
+    for (int i = 0; i < 6; i++) {
+        cout << d[i] << " ";
+    }
+    cout << "\n";
+
+    float s = 0;
+    for (int i = 0; i < 6; i++) {
+        s += d[i];
+    }
+    float avg = s / 6;
+
+    int k;
+    if (avg > 0) {
+        k = 4; // 2/3 от 6
+    }
+    else {
+        k = 2; // 1/3 от 6
+    }
+
+    // сортируем первую часть
+    for (int i = 0; i < k - 1; i++) {
+        for (int j = 0; j < k - i - 1; j++) {
+            if (d[j] > d[j + 1]) {
+                int t = d[j];
+                d[j] = d[j + 1];
+                d[j + 1] = t;
+            }
+        }
+    }
+
+    // обратный порядок для остального
+    for (int i = k; i < 5; i++) {
+        for (int j = k; j < 5; j++) {
+            if (d[j] < d[j + 1]) {
+                int t = d[j];
+                d[j] = d[j + 1];
+                d[j + 1] = t;
+            }
+        }
+    }
+
+    cout << "результат: ";
+    for (int i = 0; i < 6; i++) {
+        cout << d[i] << " ";
+    }
+    cout << "\n";
 
     return 0;
 }
