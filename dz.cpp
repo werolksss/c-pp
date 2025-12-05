ac68c5e4f22fc1606512806a66165c4af2ee3de7
werolksss <darialaf@icloud.com>
2025-12-06 01:15:20 +0900

    указатели 09.12

--- dz.cpp
+++ dz.cpp
@@ -4,156 +4,179 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "ru");
 
-    // 3
-    cout << " задание 3 \n";
-
-    int m1, n1;
-    cout << "введите размер массива a: ";
-    cin >> m1;
-    cout << "введите размер массива b: ";
-    cin >> n1;
+    //1
+    cout << "задание 1\n";
+    int m, n;
+    cout << "размер массива a: ";
+    cin >> m;
+    cout << "размер массива b: ";
+    cin >> n;
 
     // создаем массивы
-    int* a1 = new int[m1];
-    int* b1 = new int[n1];
+    int* a = new int[m];
+    int* b = new int[n];
 
-    // заполняем
-    cout << "введите " << m1 << " чисел для массива a:\n";
-    for (int i = 0; i < m1; i++) {
-        cin >> a1[i];
+    cout << "\nвведите элементы массива a:\n";
+    for (int i = 0; i < m; i++) {
+        cout << "a[" << i << "] = ";
+        cin >> a[i];
     }
-
-    cout << "введите " << n1 << " чисел для массива b:\n";
-    for (int i = 0; i < n1; i++) {
-        cin >> b1[i];
+    cout << "\nвведите элементы массива b:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "b[" << i << "] = ";
+        cin >> b[i];
     }
-
-    // временный массив для результата
-    int* temp1 = new int[m1 + n1];
     int count1 = 0;
-
-    // добавляем элементы из a
-    for (int i = 0; i < m1; i++) {
-        bool duplicate = false;
-        for (int k = 0; k < count1; k++) {
-            if (temp1[k] == a1[i]) {
-                duplicate = true;
+    for (int i = 0; i < m; i++) {
+        bool estb = false;
+        for (int j = 0; j < n; j++) {
+            if (a[i] == b[j]) {
+                estb = true;
                 break;
             }
         }
-        if (!duplicate) {
-            temp1[count1] = a1[i];
-            count1++;
+        if (!estb) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (a[i] == a[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) count1++;
         }
     }
 
-    // добавляем элементы из b
-    for (int i = 0; i < n1; i++) {
-        bool duplicate = false;
-        for (int k = 0; k < count1; k++) {
-            if (temp1[k] == b1[i]) {
-                duplicate = true;
+    int* c = new int[count1];
+    int index1 = 0;
+    for (int i = 0; i < m; i++) {
+        bool estb = false;
+        for (int j = 0; j < n; j++) {
+            if (a[i] == b[j]) {
+                estb = true;
                 break;
             }
         }
-        if (!duplicate) {
-            temp1[count1] = b1[i];
-            count1++;
+        if (!estb) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (a[i] == a[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) {
+                c[index1] = a[i];
+                index1++;
+            }
         }
     }
+    cout << "массив c: ";
+    if (count1 == 0) cout << "пуст";
+    else for (int i = 0; i < count1; i++) cout << c[i] << " ";
+    cout << "\n";
 
-    // создаем финальный массив
-    int* c1 = new int[count1];
-    for (int i = 0; i < count1; i++) {
-        c1[i] = temp1[i];
-    }
-
-    cout << "\nрезультат (все уникальные элементы из a и b):\n";
-    for (int i = 0; i < count1; i++) {
-        cout << c1[i] << " ";
-    }
-    if (count1 == 0) {
-        cout << "массивы пустые";
-    }
-    cout << "\n\n";
-
-    delete[] a1;
-    delete[] b1;
-    delete[] temp1;
-    delete[] c1;
-
-    // 4
-    cout << " задание 4 \n";
-
-    int m2;
-    cout << "введите размер массива: ";
-    cin >> m2;
-
-    // создаем массив
-    int* a2 = new int[m2];
-
-    // заполняем 
-    cout << "введите " << m2 << " чисел для массива:\n";
-    for (int i = 0; i < m2; i++) {
-        cin >> a2[i];
-    }
-
-    // выбор пользователя
-    int choice;
-    cout << "\nчто удалить?\n";
-    cout << "1 - четные числа\n";
-    cout << "2 - нечетные числа\n";
-    cout << "ваш выбор: ";
-    cin >> choice;
-
-    // временный массив для результата
-    int* temp2 = new int[m2];
+    //2
+    cout << "\nзадание 2\n";
     int count2 = 0;
 
-    // удаляем в зависимости от выбора
-    if (choice == 1) {
-        // удаляем четные (оставляем нечетные)
-        for (int i = 0; i < m2; i++) {
-            if (a2[i] % 2 != 0) {
-                temp2[count2] = a2[i];
-                count2++;
+    for (int i = 0; i < m; i++) {
+        bool estb = false;
+        for (int j = 0; j < n; j++) {
+            if (a[i] == b[j]) {
+                estb = true;
+                break;
+            }
+        }
+        if (!estb) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (a[i] == a[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) count2++;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        bool esta = false;
+        for (int j = 0; j < m; j++) {
+            if (b[i] == a[j]) {
+                esta = true;
+                break;
+            }
+        }
+        if (!esta) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (b[i] == b[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) count2++;
+        }
+    }
+
+    int* d = new int[count2];
+    int index2 = 0;
+
+    for (int i = 0; i < m; i++) {
+        bool estb = false;
+        for (int j = 0; j < n; j++) {
+            if (a[i] == b[j]) {
+                estb = true;
+                break;
+            }
+        }
+        if (!estb) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (a[i] == a[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) {
+                d[index2] = a[i];
+                index2++;
             }
         }
     }
-    else if (choice == 2) {
-        // удаляем нечетные (оставляем четные)
-        for (int i = 0; i < m2; i++) {
-            if (a2[i] % 2 == 0) {
-                temp2[count2] = a2[i];
-                count2++;
+
+    for (int i = 0; i < n; i++) {
+        bool esta = false;
+        for (int j = 0; j < m; j++) {
+            if (b[i] == a[j]) {
+                esta = true;
+                break;
+            }
+        }
+        if (!esta) {
+            bool duplikat = false;
+            for (int k = 0; k < i; k++) {
+                if (b[i] == b[k]) {
+                    duplikat = true;
+                    break;
+                }
+            }
+            if (!duplikat) {
+                d[index2] = b[i];
+                index2++;
             }
         }
     }
-    else {
-        cout << "неверный выбор!\n";
-        delete[] a2;
-        delete[] temp2;
-        return 0;
-    }
 
-    // создаем финальный массив
-    int* result = new int[count2];
-    for (int i = 0; i < count2; i++) {
-        result[i] = temp2[i];
-    }
-
-    cout << "\nмассив после удаления:\n";
-    for (int i = 0; i < count2; i++) {
-        cout << result[i] << " ";
-    }
-    if (count2 == 0) {
-        cout << "все элементы удалены";
-    }
-    cout << "\n\n";
-
-    delete[] a2;
-    delete[] temp2;
-    delete[] result;
+    cout << "массив d: ";
+    if (count2 == 0) cout << "пуст";
+    else for (int i = 0; i < count2; i++) cout << d[i] << " ";
+    cout << "\n";
+    delete[] a;
+    delete[] b;
+    delete[] c;
+    delete[] d;
 
     return 0;
 }
