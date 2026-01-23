74b08a2edd1816a688a8f4e6e49550e14d6c4703
werolksss <darialaf@icloud.com>
2026-01-23 17:03:01 +0900

    структуры 24

--- dz.cpp
+++ dz.cpp
@@ -1,120 +1,170 @@
 ﻿#include <iostream>
-#include <locale>
+#include <string>
 using namespace std;
 
-// структура видеомагазина
-struct video
-{
-    char n[50]; // название
-    char r[50]; // режиссер
-    char g[30]; // жанр
-    int p;      // популярность
-    int c;      // цена
+// 1 задание
+struct Complex {
+    double a; 
+    double b; 
 };
 
-//1 показ всех
-void show(video a[], int b)
-{
-    for (int i = 0; i < b; i++)
-    {
-        cout << "Фильм: " << a[i].n << "\n";
-        cout << "Режиссер: " << a[i].r << "\n";
-        cout << "Жанр: " << a[i].g << "\n";
-        cout << "Рейтинг: " << a[i].p << "\n";
-        cout << "Цена: " << a[i].c << "\n\n";
-    }
+Complex sum(Complex x, Complex y) {
+    Complex result;
+    result.a = x.a + y.a;
+    result.b = x.b + y.b;
+    return result;
 }
 
-//2 поиск по названию
-void find_n(video a[], int b)
-{
-    char x[50];
-    cout << "Введите название:\n";
-    cin >> x;
-    for (int i = 0; i < b; i++)
-        if (strcmp(a[i].n, x) == 0)
-            cout << "Найден: " << a[i].n << "\n\n";
+Complex diff(Complex x, Complex y) {
+    Complex result;
+    result.a = x.a - y.a;
+    result.b = x.b - y.b;
+    return result;
 }
 
-//3 поиск по жанру
-void find_g(video a[], int b)
-{
-    char x[30];
-    cout << "Введите жанр:\n";
-    cin >> x;
-    for (int i = 0; i < b; i++)
-        if (strcmp(a[i].g, x) == 0)
-            cout << a[i].n << "\n";
-    cout << "\n";
+Complex mult(Complex x, Complex y) {
+    Complex result;
+    result.a = x.a * y.a - x.b * y.b;
+    result.b = x.a * y.b + x.b * y.a;
+    return result;
 }
 
-//4 поиск по режиссеру
-void find_r(video a[], int b)
-{
-    char x[50];
-    cout << "Введите режиссера:\n";
-    cin >> x;
-    for (int i = 0; i < b; i++)
-        if (strcmp(a[i].r, x) == 0)
-            cout << a[i].n << "\n";
-    cout << "\n";
+Complex div(Complex x, Complex y) {
+    Complex result;
+    double z = y.a * y.a + y.b * y.b;
+    result.a = (x.a * y.a + x.b * y.b) / z;
+    result.b = (x.b * y.a - x.a * y.b) / z;
+    return result;
 }
 
-//5 самый популярный в жанре
-void top(video a[], int b)
-{
-    char x[30];
-    int m = -1, k = 0;
-    cout << "Введите жанр:\n";
-    cin >> x;
-    for (int i = 0; i < b; i++)
-        if (strcmp(a[i].g, x) == 0 && a[i].p > m)
-        {
-            m = a[i].p;
-            k = i;
+void printComplex(Complex c) {
+    cout << c.a;
+    if (c.b >= 0) cout << "+";
+    cout << c.b << "i\n";
+}
+
+// 2 задание
+struct Car {
+    double length;          // длина
+    double clearance;       // клиренс
+    double engineVolume;    // объем двигателя
+    double enginePower;     // мощность двигателя
+    double wheelDiameter;   // диаметр колес
+    string color;           // цвет
+    string gearbox;         // тип коробки передач
+};
+
+void setCar(Car& car) {
+    cout << "Введите длину автомобиля: ";
+    cin >> car.length;
+
+    cout << "Введите клиренс: ";
+    cin >> car.clearance;
+
+    cout << "Введите объем двигателя: ";
+    cin >> car.engineVolume;
+
+    cout << "Введите мощность двигателя: ";
+    cin >> car.enginePower;
+
+    cout << "Введите диаметр колес: ";
+    cin >> car.wheelDiameter;
+
+    cout << "Введите цвет: ";
+    cin >> car.color;
+
+    cout << "Введите тип коробки передач: ";
+    cin >> car.gearbox;
+}
+
+void showCar(Car car) {
+    cout << "Длина: " << car.length << "\n";
+    cout << "Клиренс: " << car.clearance << "\n";
+    cout << "Объем двигателя: " << car.engineVolume << "\n";
+    cout << "Мощность двигателя: " << car.enginePower << "\n";
+    cout << "Диаметр колес: " << car.wheelDiameter << "\n";
+    cout << "Цвет: " << car.color << "\n";
+    cout << "Коробка передач: " << car.gearbox << "\n\n";
+}
+
+void findColor(Car cars[], int size) {
+    string col;
+    cout << "Введите цвет для поиска: ";
+    cin >> col;
+
+    cout << "Автомобили цвета " << col << ":\n";
+    bool found = false;
+    for (int i = 0; i < size; i++) {
+        if (cars[i].color == col) {
+            showCar(cars[i]);
+            found = true;
         }
-    cout << "Самый популярный: " << a[k].n << "\n\n";
-}
-
-//6 добавление
-void add(video a[], int& b)
-{
-    cout << "Название:\n";
-    cin >> a[b].n;
-    cout << "Режиссер:\n";
-    cin >> a[b].r;
-    cout << "Жанр:\n";
-    cin >> a[b].g;
-    cout << "Рейтинг:\n";
-    cin >> a[b].p;
-    cout << "Цена:\n";
-    cin >> a[b].c;
-    b++;
-}
-
-int main()
-{
-    setlocale(LC_ALL, "ru");
-
-    video a[10] = {
-        {"Matrix","Wach","Sci",9,300},
-        {"Avatar","Cam","Sci",8,350}
-    };
-    int b = 2, c;
-
-    while (true)
-    {
-        cout << "1-показ\n2-название\n3-жанр\n4-режиссер\n5-топ\n6-добавить\n0-выход\n";
-        cin >> c;
-
-        if (c == 1) show(a, b);
-        if (c == 2) find_n(a, b);
-        if (c == 3) find_g(a, b);
-        if (c == 4) find_r(a, b);
-        if (c == 5) top(a, b);
-        if (c == 6) add(a, b);
-        if (c == 0) break;
     }
+    if (!found) {
+        cout << "Не найдено\n";
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    // 1 задание
+    cout << "Комплексные числа\n\n";
+
+    Complex c1, c2;
+
+    cout << "Введите первое комплексное число:\n";
+    cout << "Действительная часть: ";
+    cin >> c1.a;
+    cout << "Мнимая часть: ";
+    cin >> c1.b;
+
+    cout << "\nВведите второе комплексное число:\n";
+    cout << "Действительная часть: ";
+    cin >> c2.a;
+    cout << "Мнимая часть: ";
+    cin >> c2.b;
+
+    cout << "\n";
+    cout << "c1 = "; printComplex(c1);
+    cout << "c2 = "; printComplex(c2);
+
+    cout << "\nРезультаты операций:\n";
+    cout << "Сумма: "; printComplex(sum(c1, c2));
+    cout << "Разность: "; printComplex(diff(c1, c2));
+    cout << "Умножение: "; printComplex(mult(c1, c2));
+    cout << "Деление: "; printComplex(div(c1, c2));
+
+    cout << "\n\n";
+
+    // 2 задание
+    cout << "Автомобили\n\n";
+
+    int n;
+    cout << "Сколько автомобилей добавить? ";
+    cin >> n;
+
+    Car* garage = new Car[n];
+
+    for (int i = 0; i < n; i++) {
+        cout << "\nАвтомобиль " << i + 1 << ":\n";
+        setCar(garage[i]);
+    }
+
+    cout << "\nВесь гараж:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Автомобиль " << i + 1 << ":\n";
+        showCar(garage[i]);
+    }
+
+    char search;
+    cout << "Хотите найти автомобиль по цвету? (y/n): ";
+    cin >> search;
+
+    if (search == 'y' || search == 'Y') {
+        findColor(garage, n);
+    }
+
+    delete[] garage;
 
     return 0;
 }
