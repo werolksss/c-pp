ec7bc1da5c49e992462a03577eeef949fece6d99
werolksss <darialaf@icloud.com>
2026-01-23 18:20:47 +0900

    структуры 28

--- dz.cpp
+++ dz.cpp
@@ -1,170 +1,200 @@
 ﻿#include <iostream>
-#include <string>
+#include <cmath>
 using namespace std;
 
 // 1 задание
-struct Complex {
-    double a; 
-    double b; 
+struct Rectangle {
+    double x; // координата X левого верхнего угла
+    double y; // координата Y левого верхнего угла
+    double width; // ширина
+    double height; // высота
 };
 
-Complex sum(Complex x, Complex y) {
-    Complex result;
-    result.a = x.a + y.a;
-    result.b = x.b + y.b;
-    return result;
+void moveRect(Rectangle& r, double dx, double dy) {
+    r.x += dx;
+    r.y += dy;
+    cout << "Прямоугольник перемещен на (" << dx << ", " << dy << ")\n";
 }
 
-Complex diff(Complex x, Complex y) {
-    Complex result;
-    result.a = x.a - y.a;
-    result.b = x.b - y.b;
-    return result;
+void resizeRect(Rectangle& r, double newWidth, double newHeight) {
+    r.width = newWidth;
+    r.height = newHeight;
+    cout << "Размер изменен: ширина=" << newWidth << ", высота=" << newHeight << "\n";
 }
 
-Complex mult(Complex x, Complex y) {
-    Complex result;
-    result.a = x.a * y.a - x.b * y.b;
-    result.b = x.a * y.b + x.b * y.a;
-    return result;
-}
-
-Complex div(Complex x, Complex y) {
-    Complex result;
-    double z = y.a * y.a + y.b * y.b;
-    result.a = (x.a * y.a + x.b * y.b) / z;
-    result.b = (x.b * y.a - x.a * y.b) / z;
-    return result;
-}
-
-void printComplex(Complex c) {
-    cout << c.a;
-    if (c.b >= 0) cout << "+";
-    cout << c.b << "i\n";
+void printRect(Rectangle r) {
+    cout << "Прямоугольник:\n";
+    cout << "Левая верхняя точка: (" << r.x << ", " << r.y << ")\n";
+    cout << "Ширина: " << r.width << "\n";
+    cout << "Высота: " << r.height << "\n";
+    cout << "Правая нижняя точка: (" << r.x + r.width << ", " << r.y + r.height << ")\n\n";
 }
 
 // 2 задание
-struct Car {
-    double length;          // длина
-    double clearance;       // клиренс
-    double engineVolume;    // объем двигателя
-    double enginePower;     // мощность двигателя
-    double wheelDiameter;   // диаметр колес
-    string color;           // цвет
-    string gearbox;         // тип коробки передач
+struct Point {
+    double x;
+    double y;
 };
 
-void setCar(Car& car) {
-    cout << "Введите длину автомобиля: ";
-    cin >> car.length;
-
-    cout << "Введите клиренс: ";
-    cin >> car.clearance;
-
-    cout << "Введите объем двигателя: ";
-    cin >> car.engineVolume;
-
-    cout << "Введите мощность двигателя: ";
-    cin >> car.enginePower;
-
-    cout << "Введите диаметр колес: ";
-    cin >> car.wheelDiameter;
-
-    cout << "Введите цвет: ";
-    cin >> car.color;
-
-    cout << "Введите тип коробки передач: ";
-    cin >> car.gearbox;
+double distance(Point p1, Point p2) {
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return sqrt(dx * dx + dy * dy);
 }
 
-void showCar(Car car) {
-    cout << "Длина: " << car.length << "\n";
-    cout << "Клиренс: " << car.clearance << "\n";
-    cout << "Объем двигателя: " << car.engineVolume << "\n";
-    cout << "Мощность двигателя: " << car.enginePower << "\n";
-    cout << "Диаметр колес: " << car.wheelDiameter << "\n";
-    cout << "Цвет: " << car.color << "\n";
-    cout << "Коробка передач: " << car.gearbox << "\n\n";
+// 3 задание
+struct Fraction {
+    int num; 
+    int den; 
+};
+
+int findGCD(int a, int b) {
+    a = abs(a);
+    b = abs(b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-void findColor(Car cars[], int size) {
-    string col;
-    cout << "Введите цвет для поиска: ";
-    cin >> col;
+void simplify(Fraction& f) {
+    int gcd = findGCD(f.num, f.den);
+    f.num /= gcd;
+    f.den /= gcd;
 
-    cout << "Автомобили цвета " << col << ":\n";
-    bool found = false;
-    for (int i = 0; i < size; i++) {
-        if (cars[i].color == col) {
-            showCar(cars[i]);
-            found = true;
-        }
+    if (f.den < 0) {
+        f.num = -f.num;
+        f.den = -f.den;
     }
-    if (!found) {
-        cout << "Не найдено\n";
+}
+
+void toMixedNumber(Fraction f) {
+    if (abs(f.num) >= f.den && f.den != 0) {
+        int whole = f.num / f.den;
+        int remainder = abs(f.num) % f.den;
+        cout << whole << " и " << remainder << "/" << f.den;
     }
+    else {
+        cout << f.num << "/" << f.den;
+    }
+}
+
+Fraction addFractions(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.num = f1.num * f2.den + f2.num * f1.den;
+    result.den = f1.den * f2.den;
+    simplify(result);
+    return result;
+}
+
+Fraction subtractFractions(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.num = f1.num * f2.den - f2.num * f1.den;
+    result.den = f1.den * f2.den;
+    simplify(result);
+    return result;
+}
+
+Fraction multiplyFractions(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.num = f1.num * f2.num;
+    result.den = f1.den * f2.den;
+    simplify(result);
+    return result;
+}
+
+Fraction divideFractions(Fraction f1, Fraction f2) {
+    Fraction result;
+    result.num = f1.num * f2.den;
+    result.den = f1.den * f2.num;
+    simplify(result);
+    return result;
+}
+
+void printFraction(Fraction f) {
+    cout << f.num << "/" << f.den;
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
-    // 1 задание
-    cout << "Комплексные числа\n\n";
 
-    Complex c1, c2;
+    cout << "Прямоугольник\n\n";
 
-    cout << "Введите первое комплексное число:\n";
-    cout << "Действительная часть: ";
-    cin >> c1.a;
-    cout << "Мнимая часть: ";
-    cin >> c1.b;
+    Rectangle r;
+    cout << "Введите координаты левого верхнего угла прямоугольника (x y): ";
+    cin >> r.x >> r.y;
 
-    cout << "\nВведите второе комплексное число:\n";
-    cout << "Действительная часть: ";
-    cin >> c2.a;
-    cout << "Мнимая часть: ";
-    cin >> c2.b;
+    cout << "Введите ширину и высоту прямоугольника: ";
+    cin >> r.width >> r.height;
 
-    cout << "\n";
-    cout << "c1 = "; printComplex(c1);
-    cout << "c2 = "; printComplex(c2);
+    printRect(r);
 
-    cout << "\nРезультаты операций:\n";
-    cout << "Сумма: "; printComplex(sum(c1, c2));
-    cout << "Разность: "; printComplex(diff(c1, c2));
-    cout << "Умножение: "; printComplex(mult(c1, c2));
-    cout << "Деление: "; printComplex(div(c1, c2));
+    double dx, dy;
+    cout << "Введите смещение по X и Y для перемещения: ";
+    cin >> dx >> dy;
+    moveRect(r, dx, dy);
+    printRect(r);
 
-    cout << "\n\n";
+    double newW, newH;
+    cout << "Введите новую ширину и высоту: ";
+    cin >> newW >> newH;
+    resizeRect(r, newW, newH);
+    printRect(r);
 
-    // 2 задание
-    cout << "Автомобили\n\n";
+    cout << "\nРасстояние между точками\n\n";
 
-    int n;
-    cout << "Сколько автомобилей добавить? ";
-    cin >> n;
+    Point p1, p2;
+    cout << "Введите координаты первой точки (x y): ";
+    cin >> p1.x >> p1.y;
 
-    Car* garage = new Car[n];
+    cout << "Введите координаты второй точки (x y): ";
+    cin >> p2.x >> p2.y;
 
-    for (int i = 0; i < n; i++) {
-        cout << "\nАвтомобиль " << i + 1 << ":\n";
-        setCar(garage[i]);
-    }
+    double dist = distance(p1, p2);
+    cout << "Расстояние между точками: " << dist << "\n\n";
 
-    cout << "\nВесь гараж:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "Автомобиль " << i + 1 << ":\n";
-        showCar(garage[i]);
-    }
+    cout << "Дроби\n\n";
 
-    char search;
-    cout << "Хотите найти автомобиль по цвету? (y/n): ";
-    cin >> search;
+    Fraction f1, f2;
 
-    if (search == 'y' || search == 'Y') {
-        findColor(garage, n);
-    }
+    cout << "Введите первую дробь (числитель и знаменатель): ";
+    cin >> f1.num >> f1.den;
 
-    delete[] garage;
+    cout << "Введите вторую дробь (числитель и знаменатель): ";
+    cin >> f2.num >> f2.den;
+
+    simplify(f1);
+    simplify(f2);
+
+    cout << "\nСокращенные дроби:\n";
+    cout << "Дробь 1: "; printFraction(f1); cout << "\n";
+    cout << "Дробь 2: "; printFraction(f2); cout << "\n";
+
+    cout << "\nПервая дробь как смешанное число: ";
+    toMixedNumber(f1); cout << "\n";
+
+    cout << "Вторая дробь как смешанное число: ";
+    toMixedNumber(f2); cout << "\n\n";
+
+    Fraction result;
+
+    result = addFractions(f1, f2);
+    cout << "Сумма: "; printFraction(f1); cout << " + "; printFraction(f2);
+    cout << " = "; printFraction(result); cout << "\n";
+
+    result = subtractFractions(f1, f2);
+    cout << "Разность: "; printFraction(f1); cout << " - "; printFraction(f2);
+    cout << " = "; printFraction(result); cout << "\n";
+
+    result = multiplyFractions(f1, f2);
+    cout << "Умножение: "; printFraction(f1); cout << " * "; printFraction(f2);
+    cout << " = "; printFraction(result); cout << "\n";
+
+    result = divideFractions(f1, f2);
+    cout << "Деление: "; printFraction(f1); cout << " / "; printFraction(f2);
+    cout << " = "; printFraction(result); cout << "\n";
 
     return 0;
 }
