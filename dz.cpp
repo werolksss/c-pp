3eada048b084b4557b0ff9414983fde0bb0b8a34
werolksss <darialaf@icloud.com>
2026-02-11 15:02:23 +0900

    ооп введение

--- dz.cpp
+++ dz.cpp
@@ -1,121 +1,111 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <string>
-#include <locale>
+#include <windows.h>
 
 using namespace std;
 
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    // Ввод дроби
+    void input() {
+        cout << "Введите числитель: ";
+        cin >> numerator;
+        cout << "Введите знаменатель: ";
+        cin >> denominator;
+
+        if (denominator == 0) {
+            cout << "Знаменатель не может быть равен 0\n";
+            denominator = 1;
+        }
+    }
+
+    // Вывод дроби
+    void print() {
+        cout << numerator << "/" << denominator;
+    }
+
+    // Сложение
+    Fraction add(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator + f.numerator * denominator;
+        result.denominator = denominator * f.denominator;
+        return result;
+    }
+
+    // Вычитание
+    Fraction subtract(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator - f.numerator * denominator;
+        result.denominator = denominator * f.denominator;
+        return result;
+    }
+
+    // Умножение
+    Fraction multiply(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.numerator;
+        result.denominator = denominator * f.denominator;
+        return result;
+    }
+
+    // Деление
+    Fraction divide(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator;
+        result.denominator = denominator * f.numerator;
+
+        if (result.denominator == 0) {
+            cout << "\Деление на ноль невозможно\n";
+            result.denominator = 1;
+        }
+
+        return result;
+    }
+};
+
 int main() {
-    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    // 1 задание: подсчет слов, начинающихся с заданного символа
-    {
-        ifstream file1("file1.txt");
-        if (!file1.is_open()) {
-            cout << "не удалось открыть file1.txt\n";
-            return 1;
-        }
+    Fraction f1, f2, result;
 
-        char a;
-        cout << "введите символ для поиска: ";
-        cin >> a;
+    cout << "Работа с дробями\n";
 
-        int b = 0;
-        string c;
+    cout << "Введите первую дробь:\n";
+    f1.input();
 
-        while (file1 >> c) {
-            if (!c.empty() && c[0] == a) {
-                b++;
-            }
-        }
+    cout << "\nВведите вторую дробь:\n";
+    f2.input();
 
-        cout << "слов, начинающихся с '" << a << "': " << b << "\n";
-        file1.close();
-    }
+    cout << "\nРезультаты операций:\n";
 
-    // 2 задание: замена 0 на 1 и 1 на 0
-    {
-        ifstream file2("file2.txt");
-        ofstream file2_new("file2_new.txt");
+    cout << "Сложение: ";
+    result = f1.add(f2);
+    result.print();
+    cout << endl;
 
-        if (!file2.is_open() || !file2_new.is_open()) {
-            cout << "не удалось открыть файлы для 2 задания\n";
-            return 1;
-        }
+    cout << "Вычитание: ";
+    result = f1.subtract(f2);
+    result.print();
+    cout << endl;
 
-        string d;
-        while (getline(file2, d)) {
-            for (char& e : d) {
-                if (e == '0') {
-                    e = '1';
-                }
-                else if (e == '1') {
-                    e = '0';
-                }
-            }
-            file2_new << d << "\n";
-        }
+    cout << "Умножение: ";
+    result = f1.multiply(f2);
+    result.print();
+    cout << endl;
 
-        file2.close();
-        file2_new.close();
-        cout << "файл преобразован и сохранен как file2_new.txt\n";
-    }
-
-    // 3 задание: запись массива строк в файл
-    {
-        string f[] = { "первая строка", "вторая строка", "третья строка" };
-        ofstream file3("file3.txt");
-
-        if (!file3.is_open()) {
-            cout << "не удалось создать file3.txt\n";
-            return 1;
-        }
-
-        for (const string& g : f) {
-            file3 << g << "\n";
-        }
-
-        file3.close();
-        cout << "массив строк записан в file3.txt\n";
-    }
-
-    // 4 задание: подсчет символов в файле
-    {
-        ifstream file4("file4.txt");
-        if (!file4.is_open()) {
-            cout << "не удалось открыть file4.txt\n";
-            return 1;
-        }
-
-        int h = 0;
-        char i;
-
-        while (file4.get(i)) {
-            h++;
-        }
-
-        file4.close();
-        cout << "количество символов в файле: " << h << "\n";
-    }
-
-    // 5 задание: подсчет строк в файле
-    {
-        ifstream file5("file5.txt");
-        if (!file5.is_open()) {
-            cout << "не удалось открыть file5.txt\n";
-            return 1;
-        }
-
-        int j = 0;
-        string k;
-
-        while (getline(file5, k)) {
-            j++;
-        }
-
-        file5.close();
-        cout << "количество строк в файле: " << j << "\n";
-    }
+    cout << "Деление: ";
+    result = f1.divide(f2);
+    result.print();
+    cout << endl;
 
     return 0;
 }
