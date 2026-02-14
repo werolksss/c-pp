f9636a956db707a926d59ac8c705acde8840d380
werolksss <darialaf@icloud.com>
2026-02-15 01:54:21 +0900

    3) Дробь

--- dz.cpp
+++ dz.cpp
@@ -1,159 +1,191 @@
 ﻿#include <iostream>
-#include <fstream>
 #include <windows.h>
+
 using namespace std;
 
-class Point {
+class Fraction {
 private:
-    double x, y, z;
+    int numerator;
+    int denominator;
     static int objectCount;  // Статический счетчик объектов
 
 public:
-    // Конструктор по умолчанию (инициализация нулями)
-    Point() : Point(0, 0, 0) {
+    // Конструктор по умолчанию (0/1)
+    Fraction() : Fraction(0, 1) {
         cout << "Конструктор по умолчанию\n";
     }
 
-    // Конструктор с двумя координатами (третья = 0)
-    Point(double x, double y) : Point(x, y, 0) {
-        cout << "Конструктор с двумя координатами\n";
+    // Конструктор с числителем (знаменатель = 1)
+    Fraction(int num) : Fraction(num, 1) {
+        cout << "Конструктор с числителем\n";
     }
 
-    // Конструктор с одной координатой (остальные = 0)
-    Point(double x) : Point(x, 0, 0) {
-        cout << "Конструктор с одной координатой\n";
-    }
+    // Полный конструктор с числителем и знаменателем
+    Fraction(int num, int den) {
+        numerator = num;
 
-    // Полный конструктор со всеми координатами
-    Point(double x, double y, double z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
+        if (den == 0) {
+            cout << "Знаменатель не может быть 0, установлен 1\n";
+            denominator = 1;
+        }
+        else {
+            denominator = den;
+        }
+
         objectCount++;
         cout << "Полный конструктор\n";
     }
 
     // Конструктор копирования
-    Point(const Point& other) {
-        this->x = other.x;
-        this->y = other.y;
-        this->z = other.z;
+    Fraction(const Fraction& other) {
+        numerator = other.numerator;
+        denominator = other.denominator;
         objectCount++;
         cout << "Конструктор копирования\n";
     }
 
     // Деструктор
-    ~Point() {
+    ~Fraction() {
         objectCount--;
-        cout << "Деструктор для точки (" << x << ", " << y << ", " << z << ")\n";
+        cout << "Деструктор для дроби " << numerator << "/" << denominator << "\n";
     }
 
-    // Ввод данных
+    // Ввод дроби
     void input() {
-        cout << "Введите координату X: ";
-        cin >> x;
-        cout << "Введите координату Y: ";
-        cin >> y;
-        cout << "Введите координату Z: ";
-        cin >> z;
+        cout << "Введите числитель: ";
+        cin >> numerator;
+        cout << "Введите знаменатель: ";
+        cin >> denominator;
+
+        if (denominator == 0) {
+            cout << "Знаменатель не может быть равен 0\n";
+            denominator = 1;
+        }
     }
 
-    // Вывод данных
+    // Вывод дроби
     void print() {
-        cout << "Координаты точки: ("
-            << x << ", "
-            << y << ", "
-            << z << ")" << endl;
+        cout << numerator << "/" << denominator;
     }
 
-    // Аксессоры
-    double getX() { return x; }
-    double getY() { return y; }
-    double getZ() { return z; }
-
-    void setX(double newX) { x = newX; }
-    void setY(double newY) { y = newY; }
-    void setZ(double newZ) { z = newZ; }
-
-    // Сохранение в файл
-    void saveToFile(string filename) {
-        ofstream file(filename);
-        if (file.is_open()) {
-            file << x << " " << y << " " << z;
-            file.close();
-            cout << "Данные успешно сохранены в файл.\n";
-        }
-        else {
-            cout << "Ошибка при открытии файла!\n";
-        }
+    // Сложение
+    Fraction add(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator + f.numerator * denominator;
+        result.denominator = denominator * f.denominator;
+        return result;
     }
 
-    // Загрузка из файла
-    void loadFromFile(string filename) {
-        ifstream file(filename);
-        if (file.is_open()) {
-            file >> x >> y >> z;
-            file.close();
-            cout << "Данные успешно загружены из файла.\n";
+    // Вычитание
+    Fraction subtract(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator - f.numerator * denominator;
+        result.denominator = denominator * f.denominator;
+        return result;
+    }
+
+    // Умножение
+    Fraction multiply(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.numerator;
+        result.denominator = denominator * f.denominator;
+        return result;
+    }
+
+    // Деление
+    Fraction divide(Fraction f) {
+        Fraction result;
+        result.numerator = numerator * f.denominator;
+        result.denominator = denominator * f.numerator;
+
+        if (result.denominator == 0) {
+            cout << "Деление на ноль невозможно\n";
+            result.denominator = 1;
         }
-        else {
-            cout << "Ошибка при открытии файла!\n";
-        }
+
+        return result;
     }
 
     // Статический метод для получения количества объектов
     static int getObjectCount() {
         return objectCount;
     }
+
+    // Методы доступа
+    int getNumerator() { return numerator; }
+    int getDenominator() { return denominator; }
+
+    void setNumerator(int num) { numerator = num; }
+    void setDenominator(int den) {
+        if (den != 0) {
+            denominator = den;
+        }
+        else {
+            cout << "Знаменатель не может быть 0\n";
+        }
+    }
 };
 
 // Инициализация статического члена класса
-int Point::objectCount = 0;
+int Fraction::objectCount = 0;
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    cout << "Начальное количество объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Начальное количество объектов: " << Fraction::getObjectCount() << "\n\n";
 
-    cout << "Создание точки p1 (конструктор по умолчанию)\n";
-    Point p1;
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Создание дроби f1 (конструктор по умолчанию)\n";
+    Fraction f1;
+    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
 
-    cout << "Создание точки p2 (конструктор с двумя координатами)\n";
-    Point p2(5.5, 3.2);
-    p2.print();
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Создание дроби f2 (конструктор с числителем)\n";
+    Fraction f2(5);
+    cout << "f2 = "; f2.print(); cout << "\n";
+    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
 
-    cout << "Создание точки p3 (конструктор с одной координатой)\n";
-    Point p3(10.0);
-    p3.print();
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Создание дроби f3 (полный конструктор)\n";
+    Fraction f3(3, 4);
+    cout << "f3 = "; f3.print(); cout << "\n";
+    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
 
-    cout << "Создание точки p4 (полный конструктор)\n";
-    Point p4(1.1, 2.2, 3.3);
-    p4.print();
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Создание дроби f4 (копия f3)\n";
+    Fraction f4(f3);
+    cout << "f4 = "; f4.print(); cout << "\n";
+    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
 
-    cout << "Создание точки p5 (копия p4)\n";
-    Point p5(p4);
-    p5.print();
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "Ввод первой дроби:\n";
+    f1.input();
 
-    cout << "Ввод данных для точки p1:\n";
-    p1.input();
-    p1.print();
+    cout << "\nВвод второй дроби:\n";
+    Fraction f5;
+    f5.input();
 
-    cout << "\nСохранение точки p1 в файл\n";
-    p1.saveToFile("point.txt");
+    cout << "\nРезультаты операций с f1 и f5:\n";
 
-    cout << "\nЗагрузка данных из файла в новую точку p6\n";
-    Point p6;
-    p6.loadFromFile("point.txt");
-    p6.print();
-    cout << "Объектов: " << Point::getObjectCount() << "\n\n";
+    cout << "f1 = "; f1.print(); cout << ", f5 = "; f5.print(); cout << "\n";
 
-    cout << "\nВсего создано объектов: " << Point::getObjectCount() << endl;
+    cout << "Сложение: ";
+    Fraction result1 = f1.add(f5);
+    result1.print();
+    cout << endl;
+
+    cout << "Вычитание: ";
+    Fraction result2 = f1.subtract(f5);
+    result2.print();
+    cout << endl;
+
+    cout << "Умножение: ";
+    Fraction result3 = f1.multiply(f5);
+    result3.print();
+    cout << endl;
+
+    cout << "Деление: ";
+    Fraction result4 = f1.divide(f5);
+    result4.print();
+    cout << endl;
+
+    cout << "\nВсего создано объектов: " << Fraction::getObjectCount() << endl;
     cout << "Завершение программы (деструкторы будут вызваны автоматически)\n";
 
     return 0;
