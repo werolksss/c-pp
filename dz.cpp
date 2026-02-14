fd6c3eb97d332d3c915db0c2cf984bb95526c502
werolksss <darialaf@icloud.com>
2026-02-15 01:46:42 +0900

    Задание №1

--- dz.cpp
+++ dz.cpp
@@ -1,123 +1,171 @@
 ﻿#include <iostream>
-#include <cstring>
+#include <cmath>
 #include <windows.h>
 using namespace std;
 
-class String {
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+class AreaCalculator {
 private:
-    char* str;
-    size_t length;
-    static int objectCount;
+    static int calculationCount;
+    AreaCalculator() {}
 
 public:
-    String() : String(80) {
-        cout << "Конструктор по умолчанию (80 символов)\n";
+    static double triangleByBaseAndHeight(double base, double height) {
+        calculationCount++;
+        return 0.5 * base * height;
     }
 
-    explicit String(size_t size) : length(size), str(new char[size + 1]) {
-        str[0] = '\0';
-        objectCount++;
-        cout << "Конструктор с размером (" << size << " символов)\n";
+    static double triangleByThreeSides(double a, double b, double c) {
+        calculationCount++;
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            cout << "Ошибка: треугольник не существует!\n";
+            return 0;
+        }
+        double p = (a + b + c) / 2;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
-    String(const char* userStr) {
-        length = strlen(userStr);
-        str = new char[length + 1];
-        strcpy_s(str, length + 1, userStr);
-        objectCount++;
-        cout << "Конструктор с инициализацией строкой\n";
+    static double triangleByTwoSidesAndAngle(double a, double b, double angleDegrees) {
+        calculationCount++;
+        double angleRadians = angleDegrees * M_PI / 180.0;
+        return 0.5 * a * b * sin(angleRadians);
     }
 
-    String(const String& other) : length(other.length), str(new char[other.length + 1]) {
-        strcpy_s(str, other.length + 1, other.str);
-        objectCount++;
-        cout << "Конструктор копирования\n";
+    static double triangleByCoordinates(double x1, double y1, double x2, double y2, double x3, double y3) {
+        calculationCount++;
+        return 0.5 * abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)));
     }
 
-    ~String() {
-        cout << "Деструктор для строки \"" << str << "\"\n";
-        delete[] str;
-        objectCount--;
+    static double rectangle(double length, double width) {
+        calculationCount++;
+        return length * width;
     }
 
-    void input() {
-        cout << "Введите строку: ";
-        char buffer[1000];
-        cin.ignore();
-        cin.getline(buffer, 1000);
-
-        delete[] str;
-        length = strlen(buffer);
-        str = new char[length + 1];
-        strcpy_s(str, length + 1, buffer);
+    static double rectangleByDiagonalAndAngle(double diagonal, double angleDegrees) {
+        calculationCount++;
+        double angleRadians = angleDegrees * M_PI / 180.0;
+        return (diagonal * diagonal * sin(angleRadians)) / 2;
     }
 
-    void print() const {
-        cout << "Строка: \"" << str << "\" (длина: " << length << ")\n";
+    static double square(double side) {
+        calculationCount++;
+        return side * side;
     }
 
-    const char* getString() const {
-        return str;
+    static double squareByDiagonal(double diagonal) {
+        calculationCount++;
+        return (diagonal * diagonal) / 2;
     }
 
-    size_t getLength() const {
-        return length;
+    static double squareByPerimeter(double perimeter) {
+        calculationCount++;
+        double side = perimeter / 4;
+        return side * side;
     }
 
-    static int getObjectCount() {
-        return objectCount;
+    static double rhombusByDiagonals(double d1, double d2) {
+        calculationCount++;
+        return (d1 * d2) / 2;
     }
 
-    void setString(const char* newStr) {
-        delete[] str;
-        length = strlen(newStr);
-        str = new char[length + 1];
-        strcpy_s(str, length + 1, newStr);
+    static double rhombusBySideAndHeight(double side, double height) {
+        calculationCount++;
+        return side * height;
+    }
+
+    static double rhombusBySideAndAngle(double side, double angleDegrees) {
+        calculationCount++;
+        double angleRadians = angleDegrees * M_PI / 180.0;
+        return side * side * sin(angleRadians);
+    }
+
+    static double rhombusByDiagonalAndSide(double diagonal, double side) {
+        calculationCount++;
+        double secondDiagonal = 2 * sqrt(side * side - (diagonal * diagonal) / 4);
+        return (diagonal * secondDiagonal) / 2;
+    }
+
+    static int getCalculationCount() {
+        return calculationCount;
+    }
+
+    static void resetCalculationCount() {
+        calculationCount = 0;
+        cout << "Счетчик сброшен.\n";
     }
 };
 
-int String::objectCount = 0;
+int AreaCalculator::calculationCount = 0;
+
+void printResult(const string& figure, double area) {
+    cout << "Площадь " << figure << ": " << area << endl;
+}
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    cout << "Демонстрация работы класса String\n\n";
-    cout << "Начальное количество объектов: " << String::getObjectCount() << "\n\n";
+    cout << "Калькулятор площади фигур\n\n";
 
-    cout << "Создание s1 (конструктор по умолчанию)\n";
-    String s1;
-    s1.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    cout << "Треугольники\n";
+    double area = AreaCalculator::triangleByBaseAndHeight(10, 5);
+    printResult("треугольника (осн.10, выс.5)", area);
 
-    cout << "Создание s2 (конструктор с размером 20)\n";
-    String s2(20);
-    s2.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    area = AreaCalculator::triangleByThreeSides(3, 4, 5);
+    printResult("треугольника (3,4,5)", area);
 
-    cout << "Создание s3 (конструктор с инициализацией)\n";
-    String s3("Привет, мир!");
-    s3.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    area = AreaCalculator::triangleByTwoSidesAndAngle(6, 8, 30);
+    printResult("треугольника (6,8,30°)", area);
 
-    cout << "Ввод строки для s1\n";
-    s1.input();
-    s1.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    area = AreaCalculator::triangleByCoordinates(0, 0, 4, 0, 0, 3);
+    printResult("треугольника по координатам", area);
 
-    cout << "Создание копии s4 (копия s3)\n";
-    String s4(s3);
-    s4.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    cout << "\nПрямоугольники\n";
+    area = AreaCalculator::rectangle(5, 8);
+    printResult("прямоугольника (5x8)", area);
 
-    cout << "Изменение строки s2\n";
-    s2.setString("Новая строка");
-    s2.print();
-    cout << "Объектов: " << String::getObjectCount() << "\n\n";
+    area = AreaCalculator::rectangleByDiagonalAndAngle(10, 60);
+    printResult("прямоугольника (диаг.10,60°)", area);
 
-    cout << "Программа завершается\n";
-    cout << "Текущее количество объектов: " << String::getObjectCount() << "\n";
-    cout << "Уничтожение объектов:\n";
+    cout << "\nКвадраты\n";
+    area = AreaCalculator::square(7);
+    printResult("квадрата (стор.7)", area);
+
+    area = AreaCalculator::squareByDiagonal(10);
+    printResult("квадрата (диаг.10)", area);
+
+    area = AreaCalculator::squareByPerimeter(20);
+    printResult("квадрата (перим.20)", area);
+
+    cout << "\nРомбы\n";
+    area = AreaCalculator::rhombusByDiagonals(8, 6);
+    printResult("ромба (диаг.8,6)", area);
+
+    area = AreaCalculator::rhombusBySideAndHeight(5, 4);
+    printResult("ромба (стор.5,выс.4)", area);
+
+    area = AreaCalculator::rhombusBySideAndAngle(5, 60);
+    printResult("ромба (стор.5,60°)", area);
+
+    area = AreaCalculator::rhombusByDiagonalAndSide(8, 5);
+    printResult("ромба (диаг.8,стор.5)", area);
+
+    cout << "\nСтатистика\n";
+    cout << "Всего подсчетов: " << AreaCalculator::getCalculationCount() << endl;
+
+    cout << "\nДополнительные вычисления\n";
+    AreaCalculator::triangleByBaseAndHeight(12, 6);
+    AreaCalculator::rectangle(7, 9);
+    AreaCalculator::square(4);
+
+    cout << "Всего подсчетов: " << AreaCalculator::getCalculationCount() << endl;
+
+    cout << "\nСброс счетчика\n";
+    AreaCalculator::resetCalculationCount();
+    cout << "После сброса: " << AreaCalculator::getCalculationCount() << endl;
 
     return 0;
 }
