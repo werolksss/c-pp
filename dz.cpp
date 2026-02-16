2571357d120ed3370e3cb14e4a2a988cdbe4456d
werolksss <darialaf@icloud.com>
2026-02-17 01:40:42 +0900

    1)дробь

--- dz.cpp
+++ dz.cpp
@@ -1,192 +1,265 @@
 ﻿#include <iostream>
 #include <windows.h>
-
 using namespace std;
 
 class Fraction {
 private:
-    int numerator;
-    int denominator;
-    static int objectCount;  // Статический счетчик объектов
+    int numerator;      // числитель
+    int denominator;    // знаменатель
+
+    // Вспомогательная функция для нахождения НОД (наибольший общий делитель)
+    int gcd(int a, int b) const {
+        a = abs(a);
+        b = abs(b);
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Метод для сокращения дроби
+    void reduce() {
+        if (denominator == 0) {
+            cout << "Ошибка: знаменатель не может быть равен нулю!\n";
+            denominator = 1;
+        }
+
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        int divisor = gcd(numerator, denominator);
+        if (divisor > 0) {
+            numerator /= divisor;
+            denominator /= divisor;
+        }
+    }
 
 public:
-    // Конструктор по умолчанию (0/1)
-    Fraction() : Fraction(0, 1) {
-        cout << "Конструктор по умолчанию\n";
+    // Конструктор с параметрами (с использованием списка инициализации)
+    Fraction(int num, int den) : numerator(num), denominator(den) {
+        reduce();  // Сокращаем дробь при создании
     }
 
-    // Конструктор с числителем (знаменатель = 1)
-    Fraction(int num) : Fraction(num, 1) {
-        cout << "Конструктор с числителем\n";
-    }
-
-    // Полный конструктор с числителем и знаменателем
-    Fraction(int num, int den) {
-        numerator = num;
-
-        if (den == 0) {
-            cout << "Знаменатель не может быть 0, установлен 1\n";
-            denominator = 1;
-        }
-        else {
-            denominator = den;
-        }
-
-        objectCount++;
-        cout << "Полный конструктор\n";
-    }
-
-    // Конструктор копирования
-    Fraction(const Fraction& other) {
-        numerator = other.numerator;
-        denominator = other.denominator;
-        objectCount++;
-        cout << "Конструктор копирования\n";
-    }
+    // Конструктор по умолчанию (делегирование конструктору с параметрами)
+    Fraction() : Fraction(0, 1) {}
 
     // Деструктор
-    ~Fraction() {
-        objectCount--;
-        cout << "Деструктор для дроби " << numerator << "/" << denominator << "\n";
-    }
+    ~Fraction() {}
 
-    // Ввод дроби
-    void input() {
-        cout << "Введите числитель: ";
-        cin >> numerator;
-        cout << "Введите знаменатель: ";
-        cin >> denominator;
-
-        if (denominator == 0) {
-            cout << "Знаменатель не может быть равен 0\n";
-            denominator = 1;
+    // Метод для вывода дроби на экран
+    void print() const {
+        if (denominator == 1) {
+            cout << numerator;
         }
-    }
-
-    // Вывод дроби
-    void print() {
-        cout << numerator << "/" << denominator;
-    }
-
-    // Сложение
-    Fraction add(Fraction f) {
-        Fraction result;
-        result.numerator = numerator * f.denominator + f.numerator * denominator;
-        result.denominator = denominator * f.denominator;
-        return result;
-    }
-
-    // Вычитание
-    Fraction subtract(Fraction f) {
-        Fraction result;
-        result.numerator = numerator * f.denominator - f.numerator * denominator;
-        result.denominator = denominator * f.denominator;
-        return result;
-    }
-
-    // Умножение
-    Fraction multiply(Fraction f) {
-        Fraction result;
-        result.numerator = numerator * f.numerator;
-        result.denominator = denominator * f.denominator;
-        return result;
-    }
-
-    // Деление
-    Fraction divide(Fraction f) {
-        Fraction result;
-        result.numerator = numerator * f.denominator;
-        result.denominator = denominator * f.numerator;
-
-        if (result.denominator == 0) {
-            cout << "Деление на ноль невозможно\n";
-            result.denominator = 1;
-        }
-
-        return result;
-    }
-
-    // Статический метод для получения количества объектов
-    static int getObjectCount() {
-        return objectCount;
-    }
-
-    // Методы доступа
-    int getNumerator() { return numerator; }
-    int getDenominator() { return denominator; }
-
-    void setNumerator(int num) { numerator = num; }
-    void setDenominator(int den) {
-        if (den != 0) {
-            denominator = den;
+        else if (numerator == 0) {
+            cout << "0";
         }
         else {
-            cout << "Знаменатель не может быть 0\n";
+            cout << numerator << "/" << denominator;
         }
+    }
+
+    // Геттеры (для возможного использования)
+    int getNumerator() const { return numerator; }
+    int getDenominator() const { return denominator; }
+
+    // Сложение дроби с дробью
+    Fraction* add(const Fraction& other) {
+        numerator = numerator * other.denominator + other.numerator * denominator;
+        denominator = denominator * other.denominator;
+        reduce();
+        return this;
+    }
+
+    // Вычитание дроби с дробью
+    Fraction* subtract(const Fraction& other) {
+        numerator = numerator * other.denominator - other.numerator * denominator;
+        denominator = denominator * other.denominator;
+        reduce();
+        return this;
+    }
+
+    // Умножение дроби с дробью
+    Fraction* multiply(const Fraction& other) {
+        numerator *= other.numerator;
+        denominator *= other.denominator;
+        reduce();
+        return this;
+    }
+
+    // Деление дроби на дробь (дополнительно, для полноты)
+    Fraction* divide(const Fraction& other) {
+        if (other.numerator == 0) {
+            cout << "Ошибка: деление на ноль!\n";
+            return this;
+        }
+        numerator *= other.denominator;
+        denominator *= other.numerator;
+        reduce();
+        return this;
+    }
+
+    // Сложение дроби с целым числом
+    Fraction* add(int number) {
+        numerator += number * denominator;
+        reduce();
+        return this;
+    }
+
+    // Вычитание целого числа из дроби
+    Fraction* subtract(int number) {
+        numerator -= number * denominator;
+        reduce();
+        return this;
+    }
+
+    // Умножение дроби на целое число
+    Fraction* multiply(int number) {
+        numerator *= number;
+        reduce();
+        return this;
+    }
+
+    // Деление дроби на целое число (дополнительно)
+    Fraction* divide(int number) {
+        if (number == 0) {
+            cout << "Ошибка: деление на ноль!\n";
+            return this;
+        }
+        denominator *= number;
+        reduce();
+        return this;
+    }
+
+    // Создание новой дроби как результат сложения (без изменения текущей)
+    Fraction addNew(const Fraction& other) const {
+        Fraction result(
+            numerator * other.denominator + other.numerator * denominator,
+            denominator * other.denominator
+        );
+        return result;
+    }
+
+    // Создание новой дроби как результат вычитания
+    Fraction subtractNew(const Fraction& other) const {
+        Fraction result(
+            numerator * other.denominator - other.numerator * denominator,
+            denominator * other.denominator
+        );
+        return result;
+    }
+
+    // Создание новой дроби как результат умножения
+    Fraction multiplyNew(const Fraction& other) const {
+        Fraction result(
+            numerator * other.numerator,
+            denominator * other.denominator
+        );
+        return result;
     }
 };
 
-// Инициализация статического члена класса
-int Fraction::objectCount = 0;
+// Перегрузка оператора вывода для удобства
+ostream& operator<<(ostream& os, const Fraction& f) {
+    if (f.getDenominator() == 1) {
+        os << f.getNumerator();
+    }
+    else if (f.getNumerator() == 0) {
+        os << "0";
+    }
+    else {
+        os << f.getNumerator() << "/" << f.getDenominator();
+    }
+    return os;
+}
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    cout << "Начальное количество объектов: " << Fraction::getObjectCount() << "\n\n";
+    cout << "=== Класс Дробь ===\n\n";
 
-    cout << "Создание дроби f1 (конструктор по умолчанию)\n";
-    Fraction f1;
-    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
+    // Создание дробей
+    cout << "Создание дробей:\n";
+    Fraction f1(2, 4);      // Будет сокращена до 1/2
+    Fraction f2(3, 6);      // Будет сокращена до 1/2
+    Fraction f3(5, 1);      // Целое число 5
+    Fraction f4(7, 8);
 
-    cout << "Создание дроби f2 (конструктор с числителем)\n";
-    Fraction f2(5);
-    cout << "f2 = "; f2.print(); cout << "\n";
-    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
+    cout << "f1 = "; f1.print(); cout << " (2/4 сокращено)\n";
+    cout << "f2 = "; f2.print(); cout << " (3/6 сокращено)\n";
+    cout << "f3 = "; f3.print(); cout << " (5/1)\n";
+    cout << "f4 = "; f4.print(); cout << "\n\n";
 
-    cout << "Создание дроби f3 (полный конструктор)\n";
-    Fraction f3(3, 4);
-    cout << "f3 = "; f3.print(); cout << "\n";
-    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
+    // Демонстрация операций с возвратом this (цепочка вызовов)
+    cout << "Цепочка операций (f1 += f2 += f3):\n";
+    Fraction f5(1, 3);
+    cout << "f5 = "; f5.print(); cout << "\n";
 
-    cout << "Создание дроби f4 (копия f3)\n";
-    Fraction f4(f3);
-    cout << "f4 = "; f4.print(); cout << "\n";
-    cout << "Объектов: " << Fraction::getObjectCount() << "\n\n";
+    f5.add(f2)->add(f3);
+    cout << "После f5.add(f2)->add(f3): "; f5.print(); cout << "\n\n";
 
-    cout << "Ввод первой дроби:\n";
-    f1.input();
+    // Арифметические операции с дробями
+    cout << "Арифметические операции с дробями:\n";
+    Fraction f6(1, 2);
+    Fraction f7(1, 3);
 
-    cout << "\nВвод второй дроби:\n";
-    Fraction f5;
-    f5.input();
+    cout << "f6 = "; f6.print(); cout << "\n";
+    cout << "f7 = "; f7.print(); cout << "\n";
 
-    cout << "\nРезультаты операций с f1 и f5:\n";
+    Fraction f8 = f6.addNew(f7);
+    cout << "f6 + f7 = "; f8.print(); cout << "\n";
 
-    cout << "f1 = "; f1.print(); cout << ", f5 = "; f5.print(); cout << "\n";
+    Fraction f9 = f6.subtractNew(f7);
+    cout << "f6 - f7 = "; f9.print(); cout << "\n";
 
-    cout << "Сложение: ";
-    Fraction result1 = f1.add(f5);
-    result1.print();
-    cout << endl;
+    Fraction f10 = f6.multiplyNew(f7);
+    cout << "f6 * f7 = "; f10.print(); cout << "\n\n";
 
-    cout << "Вычитание: ";
-    Fraction result2 = f1.subtract(f5);
-    result2.print();
-    cout << endl;
+    // Операции с целыми числами
+    cout << "Операции с целыми числами:\n";
+    Fraction f11(3, 4);
+    cout << "f11 = "; f11.print(); cout << "\n";
 
-    cout << "Умножение: ";
-    Fraction result3 = f1.multiply(f5);
-    result3.print();
-    cout << endl;
+    f11.add(2);
+    cout << "f11 + 2 = "; f11.print(); cout << "\n";
 
-    cout << "Деление: ";
-    Fraction result4 = f1.divide(f5);
-    result4.print();
-    cout << endl;
+    f11 = Fraction(3, 4);  // Сброс
+    f11.subtract(1);
+    cout << "f11 - 1 = "; f11.print(); cout << "\n";
 
-    cout << "\nВсего создано объектов: " << Fraction::getObjectCount() << endl;
-    cout << "Завершение программы (деструкторы будут вызваны автоматически)\n";
+    f11 = Fraction(3, 4);  // Сброс
+    f11.multiply(3);
+    cout << "f11 * 3 = "; f11.print(); cout << "\n\n";
+
+    // Демонстрация сокращения дробей
+    cout << "Демонстрация автоматического сокращения:\n";
+    Fraction f12(12, 18);
+    Fraction f13(24, 36);
+    Fraction f14(-8, 12);
+    Fraction f15(5, -10);  // Знаменатель станет положительным
+
+    cout << "12/18 = "; f12.print(); cout << "\n";
+    cout << "24/36 = "; f13.print(); cout << "\n";
+    cout << "-8/12 = "; f14.print(); cout << "\n";
+    cout << "5/-10 = "; f15.print(); cout << "\n\n";
+
+    // Демонстрация конструктора по умолчанию (делегирование)
+    Fraction f16;
+    cout << "Дробь по умолчанию f16 = "; f16.print(); cout << "\n\n";
+
+    // Сложные выражения с использованием цепочек
+    cout << "Сложные выражения с цепочками:\n";
+    Fraction f17(1, 2);
+    cout << "f17 = "; f17.print(); cout << "\n";
+
+    // (f17 + 2) * 3 - 1/4
+    f17.add(2)->multiply(3)->subtract(Fraction(1, 4));
+    cout << "(f17 + 2) * 3 - 1/4 = "; f17.print(); cout << "\n";
 
     return 0;
 }
