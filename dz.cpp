a61d86d56ed6245b20e3155a7db0a8a3fe650a8e
werolksss <darialaf@icloud.com>
2026-03-23 17:01:18 +0900

    стек

--- dz.cpp
+++ dz.cpp
@@ -1,49 +1,24 @@
 ﻿#include <iostream>
+#include <string>
 #include <locale>
 using namespace std;
-
-struct Node {
-    int data;
-    Node* next = nullptr;
-    Node(int v) : data(v) {}
+struct Stack {
+    char arr[100];
+    int top = -1;
+    void push(char c) { arr[++top] = c; }
+    void pop() { if (top >= 0) top--; }
+    char peek() { return arr[top]; }
+    bool empty() { return top == -1; }
 };
-
-struct List {
-    Node* head = nullptr;
-
-    void push_front(int v) {
-        Node* n = new Node(v);
-        n->next = head;
-        head = n;
-    }
-
-    void push_back(int v) {
-        Node* n = new Node(v);
-        if (!head) { head = n; return; }
-        Node* c = head; while (c->next) c = c->next;
-        c->next = n;
-    }
-
-    void pop_front() {
-        if (head) { Node* t = head; head = head->next; delete t; }
-    }
-
-    void print() {
-        Node* c = head;
-        while (c) { cout << c->data << " "; c = c->next; }
-        cout << "\n";
-    }
-};
-
 int main() {
     setlocale(LC_ALL, "");
-
-    List l;
-    l.push_front(1);
-    l.push_back(2);
-    l.push_front(0);
-
-    cout << "Список: "; l.print();
-    l.pop_front();
-    cout << "После удаления первого элемента: "; l.print();
+    Stack s;
+    string str = "abba";
+    for (char c : str) s.push(c);
+    bool pal = true;
+    for (char c : str) {
+        if (s.peek() != c) pal = false;
+        s.pop();
+    }
+    cout << "\"" << str << "\" " << (pal ? "является палиндромом" : "не является палиндромом") << "\n";
 }
