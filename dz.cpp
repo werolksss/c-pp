ab29761fb3f8f727bcaa56ccc5f8851e34beb3d8
werolksss <darialaf@icloud.com>
2026-03-23 15:29:11 +0900

    11 встреча

--- dz.cpp
+++ dz.cpp
@@ -1,37 +1,66 @@
 ﻿#include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
 
-class Student {
-public:
-    string name;
+// 1. Среднее массива
+template <typename T>
+T avg(T arr[], int n) {
+    T sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    return sum / n;
+}
 
-    Student(string n) : name(n) {}
-};
+// 2. Линейное уравнение
+template <typename T>
+T solve(T a, T b) {
+    return -b / a;
+}
 
-class University {
-public:
-    vector<Student*> students;
-    void addStudent(Student* s) {
-        students.push_back(s);
+// 2. Квадратное уравнение
+template <typename T>
+void solve(T a, T b, T c) {
+    T d = b * b - 4 * a * c;
+
+    if (d > 0) {
+        T x1 = (-b + sqrt(d)) / (2 * a);
+        T x2 = (-b - sqrt(d)) / (2 * a);
+        cout << "Корни: " << x1 << " " << x2 << endl;
     }
-
-    void show() {
-        for (auto s : students)
-            cout << s->name << endl;
+    else if (d == 0) {
+        cout << "Один корень: " << -b / (2 * a) << endl;
     }
-};
+    else {
+        cout << "Корней нет\n";
+    }
+}
+
+// 3. Максимум
+template <typename T>
+T myMax(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+// 4. Минимум
+template <typename T>
+T myMin(T a, T b) {
+    return (a < b) ? a : b;
+}
 
 int main() {
-    Student s1("Alice");
-    Student s2("Bob");
+    setlocale(LC_ALL, "ru");
 
-    University uni;
+    int arr[] = { 1,2,3,4,5 };
 
-    uni.addStudent(&s1);
-    uni.addStudent(&s2);
+    cout << "Среднее массива: " << avg(arr, 5) << endl;
 
-    uni.show();
+    cout << "Решение линейного уравнения: " << solve(2.0, 4.0) << endl;
+
+    cout << "Решение квадратного уравнения: ";
+    solve(1.0, -3.0, 2.0);
+
+    cout << "Максимум: " << myMax(3, 7) << endl;
+    cout << "Минимум: " << myMin(3, 7) << endl;
 
     return 0;
 }
