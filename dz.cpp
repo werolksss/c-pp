e32ac10d2375bee30ee1dabc12c8af44d9537d7d
werolksss <darialaf@icloud.com>
2026-03-30 20:31:34 +0900

    Работа с потоками в C++

--- dz.cpp
+++ dz.cpp
@@ -1,98 +1,83 @@
 ﻿#include <iostream>
-#include <string>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <chrono>
 using namespace std;
 
-// 1
-namespace Geometry {
-    const double PI = 3.14159;
+//1
+class ThreadGuard {
+    thread t;
 
-    double circleArea(double r) {
-        return PI * r * r;
+public:
+    template <typename Func>
+    ThreadGuard(Func func) : t(func) {}
+
+    // запрет копирования
+    ThreadGuard(const ThreadGuard&) = delete;
+    ThreadGuard& operator=(const ThreadGuard&) = delete;
+
+    ~ThreadGuard() {
+        if (t.joinable())
+            t.join();
     }
-
-    double rectangleArea(double w, double h) {
-        return w * h;
-    }
-
-    namespace Shapes {
-        class Circle {
-            double radius;
-        public:
-            Circle(double r) : radius(r) {}
-
-            double area() {
-                return Geometry::circleArea(radius);
-            }
-        };
-    }
-}
+};
 
 // 2
-namespace Logger {
-    enum LogLevel { INFO, WARNING, ERROR };
+class AtomicCounter {
+    int value;
+    mutex mtx;
 
-    void log(LogLevel level, const string& msg) {
-        if (level == INFO) cout << "[INFO] ";
-        else if (level == WARNING) cout << "[WARNING] ";
-        else cout << "[ERROR] ";
+public:
+    AtomicCounter(int v = 0) : value(v) {}
 
-        cout << msg << endl;
+    void increment() {
+        lock_guard<mutex> lock(mtx);
+        value++;
     }
 
-    namespace FileLogger {
-        void logToFile(const string& file, const string& msg) {
-            cout << "Запись в файл " << file << ": " << msg << endl;
-        }
+    void decrement() {
+        lock_guard<mutex> lock(mtx);
+        value--;
     }
-}
 
-// 3
-
-// анонимное пространство
-namespace {
-    int counter = 0;
-
-    void normalize(double& v) {
-        if (v < 0) v = 0;
-        if (v > 1) v = 1;
-        counter++;
+    int get() {
+        lock_guard<mutex> lock(mtx);
+        return value;
     }
-}
-
-namespace Utils {
-    double clamp(double val, double min, double max) {
-        if (val < min) val = min;
-        if (val > max) val = max;
-
-        normalize(val); // используем скрытую функцию
-        return val;
-    }
-}
+};
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    //1
-    cout << "Площадь круга: " << Geometry::circleArea(5) << endl;
+    ThreadGuard t1([]() {
+        cout << "Поток 1 работает\n";
+        });
 
-    using Geometry::rectangleArea;
-    cout << "Площадь прямоугольника: " << rectangleArea(4, 6) << endl;
+    ThreadGuard t2([]() {
+        this_thread::sleep_for(chrono::seconds(1));
+        cout << "Поток 2 с задержкой\n";
+        });
 
-    Geometry::Shapes::Circle c(3);
-    cout << "Площадь (класс): " << c.area() << endl;
+    AtomicCounter counter(0);
+    vector<thread> threads;
 
-    //2
-    using namespace Logger;
+    for (int i = 0; i < 5; i++) {
+        threads.push_back(thread([&]() {
+            for (int j = 0; j < 10000; j++)
+                counter.increment();
+            }));
 
-    log(INFO, "Программа запущена");
-    log(WARNING, "Предупреждение");
-    log(ERROR, "Ошибка");
+        threads.push_back(thread([&]() {
+            for (int j = 0; j < 10000; j++)
+                counter.decrement();
+            }));
+    }
 
-    FileLogger::logToFile("log.txt", "Тест");
+    for (auto& t : threads)
+        t.join();
 
-    //3
-    double x = 1.5;
-    cout << "Clamp: " << Utils::clamp(x, 0, 1) << endl;
+    cout << "Итоговое значение: " << counter.get() << endl;
 
     return 0;
 }
